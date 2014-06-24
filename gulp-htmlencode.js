c0adfdd25db8b2b9df23efbe2075531d06705770
yunhua.xiao <yunhua.xiao@dianping.com>
2014-06-24 21:24:28 +0800

    init

---
+++ gulp-htmlencode.js
@@ -0,0 +1,35 @@
+var through2 = require('through2');
+var gutil = require('gulp-util');
+var PluginError = gutil.PluginError;
+var path = require('path');
+var _ = require('underscore');
+var htmlencode = require('htmlencode');
+
+module.exports = function (options) {
+  // Mixes in default options.
+  options = options || {};
+
+  function transform (file, enc, next) {
+    var self = this;
+
+    if (file.isNull()) {
+      this.push(file); // pass along
+      return next();
+    }
+
+    if (file.isStream()) {
+      this.emit('error', new PluginError('gulp-htmlencode', 'Streaming not supported'));
+      return next();
+    }
+
+    var str = file.contents.toString('utf8');
+
+    file.contents = new Buffer(str);
+    file.path = gutil.replaceExtension(file.path, '.css');
+    self.push(file);
+
+    next();
+  }
+
+  return through2.obj(transform);
+}
