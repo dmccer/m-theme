fae4501bd4493a1027f9fafc990be942dc203d35
yunhua.xiao <yunhua.xiao@dianping.com>
2014-06-25 10:58:11 +0800

    preview

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var path = require('path');
+
+gulp.task('less', function() {
+  gulp.src('./component/**/*.less')
+    .pipe(less({
+      paths: [path.join(__dirname, 'less/common/base'), path.join(__dirname, 'less/common/component')]
+    }))
+    .pipe(gulp.dest(function (file) {
+      return file.base.replace(file.cwd, '.');
+    }));
+
+  gulp.src('./common/img/**/*.*')
+    .pipe(gulp.dest('./component/img/'));
+});
+
+gulp.task('default', ['less']);
