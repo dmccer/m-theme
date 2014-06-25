1f4b1e36d8652ca5e8e3ba53029acd48a2895917
yunhua.xiao <yunhua.xiao@dianping.com>
2014-06-26 00:23:59 +0800

    bash

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+cd /home/e2f/biz-static/apollo-theme
 git pull origin master
--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+#/home/e2f/biz-static/apollo-theme/install.sh
 ssh -t e2f@192.168.8.174 '/home/e2f/biz-static/apollo-theme/install.sh'
